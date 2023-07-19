e461ccc80f61576af13a76f21351e577115850fe
Algo Hussle <citizensofandrometta@gmail.com>
2023-07-19 17:49:04 -0500

    updated links

--- .history/src/app/page_20230719174838.tsx
+++ .history/src/app/page_20230719174838.tsx
@@ -48,7 +48,7 @@ const Main: FC = () => {
                 data-aos="fade-right"
                 data-aos-delay="50"
                 data-aos-duration="1000"
-                href="/"
+                href="https://github.com/Soheib-Kiani/landwind/tree/patest"
                 className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
               >
                 <div className="flex items-center  ">
