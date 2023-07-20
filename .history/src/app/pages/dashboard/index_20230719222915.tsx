21d4f6c4ff69c5e1b357c8f44f69a03bc3e22c1e
Algo Hussle <citizensofandrometta@gmail.com>
2023-07-19 22:29:59 -0500

    updated structure

---
+++ .history/src/app/pages/dashboard/index_20230719222915.tsx
@@ -0,0 +1,14 @@
+// In dashboard/pages/index.tsx
+import React from 'react';
+import TokenChart from './components/TokenChart';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <TokenChart />
+    </div>
+  );
+};
+
+export default DashboardPage;
