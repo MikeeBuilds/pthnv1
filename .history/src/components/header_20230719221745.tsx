f038f3e9a08d6335af5a87756a3e0b5dfccd32f9
Algo Hussle <citizensofandrometta@gmail.com>
2023-07-19 22:23:44 -0500

    dashboard changes

--- .history/src/components/header_20230719221745.tsx
+++ .history/src/components/header_20230719221745.tsx
@@ -28,7 +28,7 @@ const Header: FC = () => {
                 Log in
               </div>
             </Link>
-            <Link href="dashboard/pages/index.tsx">
+            <Link href="/app/">
               <div className="cursor-pointer text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
                 Launch App
               </div>
